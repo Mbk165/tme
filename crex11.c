e206ed2fe3014d92ad8de44327b0797d956b8e0d
Mbk165 <mohamedkadri165@gmail.com>
2025-02-04 15:03:17 +0100

    Update crex11.c

--- crex11.c
+++ crex11.c
@@ -1,2 +1,40 @@
 #include <stdio.h>
 #include <assert.h>
+
+int jours(int nc, int ptotale, float pai){
+    /*Hyp: 0<=pai<=100 */
+    int c = 1;
+    float pourcent = (c / ptotale) * 100;
+    int i = 0;
+    while (pourcent <= pai){
+        c = c + nc*c;
+        pourcent = (c / ptotale) * 100;
+        i++;
+    }
+   
+    //printf("nombre de jours pour que %.2f pourcent de la population soit contaminée = %d\n", pai, i);
+    return i;
+}
+
+float pourcentage(int nc, int ptotale, int nj){
+    /*Hyp: */
+    int c = 1;
+    float pourcent = ((1.*c) / ptotale)*100;
+    int i = 1;
+    while (i <= nj){
+        c = c + nc*c;
+        pourcent = ((1.*c) / ptotale)*100;
+        i++;
+    }
+   
+    //printf("pourcentage population contaminée au bout de %d jours = %.2f \n", nj, pourcent);
+    return pourcent;
+}
+
+
+int main(){
+    assert(jours(5,10000,100.00) == 6);
+    //assert(pourcentage(5,10000,2) == 0.36);
+    pourcentage(5,10000,3);
+    return 0;
+}
